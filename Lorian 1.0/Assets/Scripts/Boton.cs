6d90d32f0372e62bf9aa1153912a9d4799340131
XLordDeusX <wilfredoa.rukoz@gmail.com>
2022-10-05 18:40:02 -0300

    Botones y puentes

---
+++ Lorian 1.0/Assets/Scripts/Boton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton : MonoBehaviour
+{
+    public GameObject buttonActive, buttonInactive;
+
+    /*private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+    }*/
+    public void Activation()
+    {
+        
+    }
+}

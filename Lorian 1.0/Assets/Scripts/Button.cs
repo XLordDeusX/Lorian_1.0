c82b7193372ba9b7a0dcb874112da0d16a90c3f8
XLordDeusX <wilfredoa.rukoz@gmail.com>
2022-10-07 01:23:45 -0300

    botones, puentes y luces

---
+++ Lorian 1.0/Assets/Scripts/Button.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject buttonActive, 
+                      buttonInactive, 
+                      bridgeActive, 
+                      bridgeInactive,
+                      lightActive,
+                      lightInactive;
+
+    private void OnTriggerEnter2D(Collider2D pushing)
+    {
+        MovableObject statueOnButton = pushing.GetComponent<MovableObject>();
+
+        if(statueOnButton != null)
+        {
+            buttonInactive.SetActive(false);
+            bridgeInactive.SetActive(false);
+            lightInactive.SetActive(false);
+            buttonActive.SetActive(true);
+            bridgeActive.SetActive(true);
+            lightActive.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D notPushing)
+    {
+        MovableObject statueAway = notPushing.GetComponent<MovableObject>();
+
+        if(statueAway != null)
+        {
+            buttonInactive.SetActive(true);
+            bridgeInactive.SetActive(true);
+            lightInactive.SetActive(true);
+            buttonActive.SetActive(false);
+            bridgeActive.SetActive(false);
+            lightActive.SetActive(false);
+        }
+    }
+}
